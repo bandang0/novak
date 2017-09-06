c8ab69a39dd24e5df5aad74b379d91e88fef1a7b
bandang0 <bandang0@yahoo.com>
2017-09-06 11:00:59 +0200

    Try dynamic allocation.

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -5,6 +5,22 @@
 using namespace std;
 
 int main(void) {
-	cout << "Hello World!" << endl;
+	cout << "Welcome to TP1." << endl;
+
+	/* Ask for array size */
+	int size;
+	cout << "Enter a size for the array:";
+	cin >> size;
+	
+	/* Allocate size for array */
+	int* tab;
+	tab = new int[size];
+
+	/* Print random stuff */
+	cout << "Here's what's in the array:\n";
+	for (int i = 0; i < size ; i++){
+		cout << "At " << tab + i << ": " << *(tab + i) << endl;
+	}
+		
 	return 0;
 }
